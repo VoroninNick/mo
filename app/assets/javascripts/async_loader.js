051becc32fc07157b535407fe3a7251ea3401fb8
Nazariy <nazariy.papizh@gmail.com>
2014-11-07 16:12:16 +0200

    reload cart

---
+++ app/assets/javascripts/async_loader.js
@@ -0,0 +1,79 @@
+//(function ($) {
+
+
+function loadPartials() {
+    var partial_paths = []
+    var partials = []
+    var $partial_containers = $('div.async-partial-loader')
+    $partial_containers.each(function () {
+        var $container = $(this)
+        var path = $container.attr('data-path')
+        partial_paths.push(path)
+        partials.push({container: $container, path: path})
+
+    })
+
+    $.ajax('?load_partial=' + partial_paths.join(),
+        {
+            dataType: 'json', type: 'GET',
+            success: function (data) {
+                var $data = $(data)
+                var response_data = $data
+                $.each(response_data, function (index, partial) {
+                    PARTIAL = partial
+                    var source = partial['partials'][0]['partial']
+                    //console.log('success. data: ', partial['partials'][0]['partial'])
+                    var $container = $partial_containers.eq(index)
+                    $container.children().remove()
+                    $container.append(source)
+
+                })
+
+                //$container.append(partial_source)
+            }
+        })
+}
+
+loadPartials()
+
+
+
+
+var loadControllerAction = function(){
+    var $partial_containers = $('div.async-loader')
+    $partial_containers.each(function () {
+        var $container = $(this)
+        //var path = $container.attr('data-path')
+        //partial_paths.push(path)
+        //partials.push({container: $container, path: path})
+
+        var url = $container.attr('data-url')
+
+
+
+        $.ajax( url+'?modal=true',
+            { dataType: 'text', type: 'GET',
+                success: function (data) {
+                    var $data = $(data)
+                    var response_data = $data
+                    var html = $data.get()
+
+                    CONTENT = data
+//                    $.each(response_data, function (index, partial) {
+//                        PARTIAL = partial
+//                        $partial_containers.eq(index).append(partial['partial'])
+//
+//                    })
+                    $container.append(data)
+
+                    $container.addClass('content-loaded')
+
+                    $container.trigger('content_loaded')
+
+                    //$container.append(partial_source)
+                }
+            })
+    })
+}
+
+loadControllerAction()
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,4 +20,54 @@ class ApplicationController < ActionController::Base
     session[:cart_id] = cart.id
     cart
   end
+
+  before_filter :detect_partial_render
+
+  def detect_ajax
+    if params[:ajax] == 'true'
+      #default_layout = 'ap'
+      #render inline: params.inspect
+
+      #render inline: controller_class_name
+      if !controller_class.nil?
+        #render inline: 'hello'
+        controller_class.layout false
+      end
+      #render layout: 'modal-layout'
+      #render inline: params.inspect
+      @modal_id = "#{params[:controller].parameterize.underscore}-#{params[:action]}-#{@_request.method.downcase}"
+
+    else
+      if !controller_class.nil?
+        #render inline: 'hello'
+        controller_class.layout 'application'
+      end
+
+
+
+    end
+  end
+
+  def detect_partial_render
+    if params[:load_partial]
+      output_data = {}
+
+      params_partial_path = params[:load_partial]
+      params_partial_paths = params_partial_path.split(',')
+      output_partials = []
+      params_partial_paths.each do |partial_path|
+        content = render_to_string layout: 'partial-loader', template: 'async_renderer/index', locals: { partial_path: partial_path }
+
+        data_to_output = { partial: content, partial_path: partial_path }
+        output_partials.push data_to_output
+      end
+
+      output_data[:partials] = output_partials
+
+
+      json_source = output_data.to_json
+      render inline: json_source
+    end
+  end
+
 end
